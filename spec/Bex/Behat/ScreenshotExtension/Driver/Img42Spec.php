c377f9ee4ea1e3e805ca2d7f0f6600414d06578f
Tibor Kotosz <tkotosz@inviqa.com>
2015-11-13 22:20:29 +0100

    Fix phpspec test

--- spec/Bex/Behat/ScreenshotExtension/Driver/Img42Spec.php
+++ spec/Bex/Behat/ScreenshotExtension/Driver/Img42Spec.php
@@ -20,7 +20,7 @@ class Img42Spec extends ObjectBehavior
 
     function it_should_call_the_api_with_the_correct_data(ContainerBuilder $container, Img42Api $api)
     {
-        $api->call('imgdata', 'img_file_name.png')->shouldBeCalled()->willReturn('imgurl');
+        $api->call('imgdata')->shouldBeCalled()->willReturn('imgurl');
         $this->upload('imgdata', 'img_file_name.png')->shouldReturn('imgurl');
     }
 }
